fe3ee1b18b9ad492a7c1358007ba17991071de87
Sai Manikanta Madhavarapu <madhavarapu.saimanikanta@gmail.com>
2025-09-12 17:55:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Category from "./pages/Category";
+import Expense from "./pages/Expense";
+import Filter from "./pages/Filter";
+import Home from "./pages/Home";
+import Income from "./pages/Income";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+const App = () => {
+  return (
+    <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/filter" element={<Filter />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
---
+++ src/pages/Income.jsx
@@ -0,0 +1,10 @@
+
+const Income = () => {
+    return (
+        <div>
+            Income
+        </div>
+    )
+}
+
+export default Income;
